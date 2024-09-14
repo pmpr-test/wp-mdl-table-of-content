9ea14cad322aef761ac5f6f71bd766849e6aca77
pmpr.dev <pmpr.dev@gmail.com>
2024-09-14 20:06:54 +0000

    Obfuscated

--- build/js/editor.MrxaSVtR4.js
+++ build/js/editor.MrxaSVtR4.js
@@ -1,2 +1,2 @@
-/*! For license information please see editor.zHKOJipf-.js.LICENSE.txt */
+/*! For license information please see editor.MrxaSVtR4.js.LICENSE.txt */
 !function(){const e=PRHelper.getHook(),t=(PRHelper.getType(),PRHelper.getHTML(),PRBackendHelper.getForm());e.on("click","#table_of_content_translate_heading_anchors",(e=>{t.getEditorContent("content")}))}();
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             66e5e7cb3da02             |
+    |             66e5ecbacd0be             |
     |_______________________________________|
 */
  use Pmpr\Module\TableOfContent\TableOfContent; TableOfContent::symcgieuakksimmu();
